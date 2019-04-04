dca5bafb30470f35e855b08125cd71d7ef18400f
donginkim <needshield@gmail.com>
2019-04-04 13:54:26 +0900

    Initialize Project
    (cherry picked from commit 68671fb8f4d66c664d1a38007382ed4bb90ea6c3)

--- src/main/java/com/buong/springwebservice/web/WebRestController.java
+++ src/main/java/com/buong/springwebservice/web/WebRestController.java
@@ -1,5 +1,6 @@
 package com.buong.springwebservice.web;
 
+<<<<<<< HEAD
 import com.buong.springwebservice.domain.posts.PostsRepository;
 import com.buong.springwebservice.dto.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
@@ -13,13 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebRestController {
     private PostsRepository postsRepository;
 
+=======
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+>>>>>>> 68671fb... Initialize Project
     @GetMapping("/hello")
     public String hello() {
         return "Hello Spring Boot!";
     }
+<<<<<<< HEAD
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
         postsRepository.save(dto.toEntity());
     }
+=======
+>>>>>>> 68671fb... Initialize Project
 }
